f0ac116ed6b4a84dc8e7d5cf4fd24d206014cd17
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2020-07-15 20:48:14 +0200

    Add some more music questions

--- quizbot/game/topics/music.py
+++ quizbot/game/topics/music.py
@@ -26,3 +26,17 @@ def whatAlbumByYear(a):
     albums = pandas.concat([right, a.sample(answersCount() - 1, weights=weights)])
     answers = albums.apply(lambda row: f'"{row.album}" by "{row.artist}"', axis=1)
     return f'What album was pubblicated in "{year}"?', tuple(answers)
+
+
+@question('music', 0, ['music/albums', 'music/artists'])
+def whatArtistByAlbum(al, ar):
+    album, artist, year = al.sample(1)[['album', 'artist', 'year']].iloc[0]
+    artists = ar[ar.artist != artist].sample(answersCount() - 1).artist
+    return f'Who performed the album "{album}" in {year}?', (artist, *artists)
+
+
+@question('music', 0, ['music/albums'])
+def whatAlbumByArtist(a):
+    album, artist, year = a.sample(1)[['album', 'artist', 'year']].iloc[0]
+    albums = a[a.artist != artist].sample(answersCount() - 1).album
+    return f'What album was made by "{artist}" in {year}?', (album, *albums)
